a30634ab82a0ebe614b2e9b0ab7c9c4351cd3fce
Tititesouris <tititesouris@laposte.net>
2018-06-19 10:30:27 +0200

    Graphs

--- visualisor.py
+++ visualisor.py
@@ -1,7 +1,6 @@
 import os
 import json
 import matplotlib.pyplot as plt
-import numpy as np
 
 data = {}
 counts = {}
@@ -15,7 +14,8 @@ for country in os.listdir("analysed/"):
         with open("analysed/" + country + "/" + filename, "r") as f:
             target = filename.strip(".json")
             counts[country][target] = 0
-            othersImage[country][target] = {"score": 0, "negative": 0, "positive": 0, "neutral": 0}
+            if country != target:
+                othersImage[country][target] = {"score": 0, "negative": 0, "positive": 0, "neutral": 0}
             for line in f:
                 counts[country][target] += 1
                 polarity = float(json.loads(line)["sentiment"]["polarity"])
@@ -35,14 +35,12 @@ with open("visualisations/data.json", "w+") as file:
 # Display settings
 barWidth = 0.35
 countryKeys = ["austria", "croatia", "france", "uk", "usa"]
-countries = ["Austria", "Croatia", "France", "United Kingdom", "USA"]
+countries = ["Austria", "Croatia", "France", "the UK", "the USA"]
 colors = ["green", "brown", "blue", "purple", "red"]
-OneToFive = [1, 2, 3, 4, 5]
+OneToFive = [(i + 1) * (barWidth * 4) for i in range(5)]
+happinessReport = [7.139, 5.321, 6.489, 6.814, 6.886]
 
-plt.title("Number of tweets about countries per country")
-plt.xlabel("Country of origin")
-plt.ylabel("Number of tweets")
-plt.xticks(OneToFive, countries)
+# Counts for each country for each target
 
 x = [[data["counts"][country][target] if country in data["counts"].keys() and target in data["counts"][
     country].keys() else 0 for country in countryKeys] for target in countryKeys]
@@ -53,34 +51,66 @@ for i in range(len(x)):
     else:
         plt.bar(OneToFive, x[i], barWidth, color=colors[i])
 
+plt.title("Number of tweets about countries per country")
+plt.xlabel("Country of origin")
+plt.ylabel("Number of tweets")
+plt.xticks(OneToFive, countries)
 plt.legend(countries)
 plt.savefig("visualisations/count_tweets")
 
-# TODO
-# For each country get score and divide by (number of pos + number of neg), compare this number between countries, and then see if the order is similar to the happiness report, see if the differences are similar as well
+# What do countries think of themselves
 
-
-exit()
-N = 5
-men_means = (20, 35, 30, 35, 27)
-men_std = (2, 3, 4, 1, 2)
-
-ind = np.arange(N)  # the x locations for the groups
-width = 0.35  # the width of the bars
+x = [[data["self_image"][country][aspect] if country in data["self_image"].keys() else 0 for country in countryKeys] for
+     aspect in ["positive", "negative", "neutral"]]
 
 fig, ax = plt.subplots()
-rects1 = ax.bar(ind, men_means, width, color='r', yerr=men_std)
+ax.bar([x - barWidth for x in OneToFive], x[0], barWidth, color="green")
+ax.bar([x for x in OneToFive], x[1], barWidth, color="red")
+ax.bar([x + barWidth for x in OneToFive], x[2], barWidth, color="blue")
 
-women_means = (25, 32, 34, 20, 25)
-women_std = (3, 5, 2, 3, 3)
-rects2 = ax.bar(ind + width, women_means, width, color='y', yerr=women_std)
+plt.title("Number of tweets with sentiment per country about themselves")
+plt.xlabel("Country")
+plt.ylabel("Number of tweets")
+plt.xticks(OneToFive, countries)
+plt.legend(["Positive", "Negative", "Neutral"])
+plt.savefig("visualisations/self_image")
 
-# add some text for labels, title and axes ticks
-ax.set_ylabel('Scores')
-ax.set_title('Scores by group and gender')
-ax.set_xticks(ind + width / 2)
-ax.set_xticklabels(('G1', 'G2', 'G3', 'G4', 'G5'))
+# Comparison with World Happiness Report
 
-ax.legend((rects1[0], rects2[0]), ('Men', 'Women'))
+x = [10 * data["self_image"][country]["score"] / (
+        data["self_image"][country]["positive"] + data["self_image"][country]["negative"]) if country in data[
+    "self_image"].keys() else 0 for country in countryKeys]
 
-plt.show()
+fig, ax = plt.subplots()
+ax.bar([x - barWidth / 2 for x in OneToFive], x, barWidth, color="blue")
+ax.bar([x + barWidth / 2 for x in OneToFive], happinessReport, barWidth, color="green")
+
+plt.title("Happiness from tweets compared to the World Happiness Report")
+plt.xlabel("Country")
+plt.ylabel("Happiness score")
+plt.xticks(OneToFive, countries)
+plt.legend(["Happiness from tweets", "World Happiness Report"])
+plt.savefig("visualisations/happiness_report")
+
+# What do countries think of each other
+
+for i, country in enumerate(countryKeys):
+    countryKeysWithoutThisOne = countryKeys[:]
+    countryKeysWithoutThisOne.pop(i)
+    countriesWithoutThisOne = countries[:]
+    countriesWithoutThisOne.pop(i)
+    x = [[data["others_image"][country][target][aspect] if country in data["others_image"].keys() and target in
+                                                           data["others_image"][country].keys() else 0 for target in
+          countryKeysWithoutThisOne] for aspect in ["positive", "negative", "neutral"]]
+
+    fig, ax = plt.subplots()
+    ax.bar([x - barWidth for x in OneToFive[:-1]], x[0], barWidth, color="green")
+    ax.bar([x for x in OneToFive[:-1]], x[1], barWidth, color="red")
+    ax.bar([x + barWidth for x in OneToFive[:-1]], x[2], barWidth, color="blue")
+
+    plt.title("Number of tweets with sentiment from " + countries[i] + " about other countries")
+    plt.xlabel("Country")
+    plt.ylabel("Number of tweets")
+    plt.xticks(OneToFive[:-1], countriesWithoutThisOne)
+    plt.legend(["Positive", "Negative", "Neutral"])
+    plt.savefig("visualisations/others_image_" + country)
